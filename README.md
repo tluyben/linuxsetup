f3fcda59066026188f4534915e4149a2599cfd9d
Tycho Luyben <tycho@e-lab.nl>
2019-06-22 19:56:34 +0200

    brightness and nicer i3 config added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # linuxsetup
+
+
+Clone and run ./install.sh in this directory; give password to sudo when requested. 
+
+
+Brightness / backlight uses this script: 
+
+git@github.com:philippnormann1337/xrandr-brightness-script.git
---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+apt install -y i3 
+
+cd /tmp; git clone git@github.com:philippnormann1337/xrandr-brightness-script.git; cd -
+
+sudo cp /tmp/xrandr-brightness-script/brightness.sh /usr/local/bin/brightness
+sudo chmod 755 /usr/local/bin/brightness
+
+cp -r .vimrc ~/
+
+sudo cp i3status.conf /etc/
+cp config ~/.config/i3/
