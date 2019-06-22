3122f8e55fb33d00bc812c53ff919617a0c10c0e
Tycho Luyben <tycho@e-lab.nl>
2019-06-22 21:03:33 +0200

    showing the curren brightness in the status bar

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ cd /tmp; git clone git@github.com:philippnormann1337/xrandr-brightness-script.gi
 sudo cp /tmp/xrandr-brightness-script/brightness.sh /usr/local/bin/brightness
 sudo chmod 755 /usr/local/bin/brightness
 
+cp customstatus ~/
+chmod 755 ~/customstatus
+cp getbrightness ~/
+chmod 755 ~/getbrightness 
+
 cp -r .vimrc ~/
 
 sudo cp i3status.conf /etc/
